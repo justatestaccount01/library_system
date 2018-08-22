e9acb8a74a4599b67308fa81b59cd1b5a6fad67e
justatestaccount01 <42400902+justatestaccount01@users.noreply.github.com>
2018-08-22 16:47:31 +0900

    Removing ganache config from truffle.js
    So that the test command runs using truffle develop not an unavailable node.

--- truffle.js
+++ truffle.js
@@ -13,14 +13,6 @@
  */
 
 module.exports = {
-	networks: {
-		development: {
-			host: "127.0.0.1",
-			port: 7545,
-			network_id: "*",
-			gas: 4612388
-		}
-	}
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
 };
